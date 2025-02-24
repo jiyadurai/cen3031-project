c0638bfb33541aea59744348fb25ff66e8807e81
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-02-24 12:14:01 -0500

    Added Daisy UI and test component

--- gatorpulse/src/App.jsx
+++ gatorpulse/src/App.jsx
@@ -1,9 +1,14 @@
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
+    <div className="bg-white flex h-screen w-screen items-center justify-center">
+      <h1 className="text-3xl text-black font-bold underline mr-2">
         Hello world! This is GatorPulse
       </h1>
+      <div>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        DaisyUI Button
+      </button>
+      </div>
     </div>
   )
 }
