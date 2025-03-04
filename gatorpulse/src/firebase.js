c67273aa41a24e3a1cee4508b5129d31cc5400e1
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-03-04 12:10:46 -0500

    Removed uneeded analytics export

--- gatorpulse/src/firebase.js
+++ gatorpulse/src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { collection, addDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 export { db, collection, addDoc };
