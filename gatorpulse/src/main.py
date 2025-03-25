9ba27b96e8552a2b1830198f05cc3a89b45b09b7
lamukaseedu <nguyenlam@ufl.edu>
2025-03-24 21:12:01 -0400

    Introduced frontend communication, demo functionality with login and signup modals

--- gatorpulse/src/main.py
+++ gatorpulse/src/main.py
@@ -1,18 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, origins="*")
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate('../servicekey.json') # Obtain private key from firebase and rename it to "servicekey.json"
+cred = credentials.Certificate('./gatorpulse/servicekey.json') # Obtain private key from firebase and rename it to "servicekey.json"
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 ref = db.collection("users")
-
+'''
 print("Adding users to database, type \"end\" to end process.")
 
 while True:
@@ -27,3 +29,28 @@ while True:
         "email": email,
         "name": name
     })
+'''
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    print(data.get("username"), data.get("password"))
+    return jsonify({"message": "success!"})
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    print(data.get("username"), data.get("password"), data.get("email"))
+
+    # Need to add encryption and email verification
+
+    ref.add({
+        "createdAt": firestore.SERVER_TIMESTAMP,
+        "email": data.get("email"),
+        "username": data.get("username"),
+        "password": data.get("password")
+    })
+    return jsonify({"message": "success!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
