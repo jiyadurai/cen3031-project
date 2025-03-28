cc1a39e6bc5a36f2c61900097ca8481f5863d382
lamukaseedu <nguyenlam@ufl.edu>
2025-03-28 14:01:17 -0400

    Implemented user verification by checking input with database

--- gatorpulse/src/main.py
+++ gatorpulse/src/main.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_cors import CORS
+from google.cloud.firestore_v1 import FieldFilter
 
 app = Flask(__name__)
 cors = CORS(app, origins="*")
@@ -14,28 +15,26 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 ref = db.collection("users")
-'''
-print("Adding users to database, type \"end\" to end process.")
-
-while True:
-    email = input("Enter your email.\n")
-    if email == "end":
-        break
-    name = input("Enter your name.\n")
-    if name == "end":
-        break
-    ref.add({
-        "createdAt": firestore.SERVER_TIMESTAMP,
-        "email": email,
-        "name": name
-    })
-'''
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    print(data.get("username"), data.get("password"))
-    return jsonify({"message": "success!"})
+
+    usernameCheck = ref.where(filter=FieldFilter("username", "==", data.get("username"))).get()
+    passCheck = ref.where(filter=FieldFilter("username", "==", data.get("username"))).get()
+
+
+    if len(usernameCheck) == 0:
+        print("Username doesn't exist!")
+        return jsonify({"message": "failure!"})
+    else:
+        passcode = passCheck[0].to_dict().get("password")
+        if data.get("password") != passcode:
+            print("Password doesn't match!")
+            return jsonify({"message": "failure!"})
+        print("Logging in...")
+        return jsonify({"message": "success!"})
+
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -43,14 +42,21 @@ def signup():
     print(data.get("username"), data.get("password"), data.get("email"))
 
     # Need to add encryption and email verification
+    usernameCheck = ref.where(filter=FieldFilter("username", "==", data.get("username"))).get()
+    emailCheck = ref.where(filter=FieldFilter("email", "==", data.get("email"))).get()
 
-    ref.add({
-        "createdAt": firestore.SERVER_TIMESTAMP,
-        "email": data.get("email"),
-        "username": data.get("username"),
-        "password": data.get("password")
-    })
-    return jsonify({"message": "success!"})
+    if len(usernameCheck) > 0:
+        print("Username already exists!")
+        return jsonify({"message": "failure!"})
+    else:
+        print("Creating new user...")
+        ref.add({
+            "createdAt": firestore.SERVER_TIMESTAMP,
+            "email": data.get("email"),
+            "username": data.get("username"),
+            "password": data.get("password")
+        })
+        return jsonify({"message": "success!"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
