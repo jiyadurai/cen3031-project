be605c0763dd2e5a01e9249ac5a08f9bc9b05356
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-04-14 11:54:08 -0400

    Added new logo's and new files for post and feed page

---
+++ gatorpulse/src/components/EventBox.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventBox() {
+  return (
+    <div>EventBox</div>
+  )
+}
--- gatorpulse/src/components/Header.jsx
+++ gatorpulse/src/components/Header.jsx
@@ -1,12 +1,12 @@
 // import React from 'react'
-import HomeLogo from '../../public/uflogo_cropped.jpg'
+import HomeLogo from '../../public/GatorPulseLogo2.png'
 
 export default function Header() {
   return (
     <section className="flex flex-row justify-between bg-white h-[7.38vh] fixed w-full z-[1] top-[0%] shadow-md">
       <div className="flex items-center ml-[2%]">
         <a href='/'>
-          <img className="w-[4vw] h-[3.3vw] mr-[10px]" src={HomeLogo}></img>
+          <img className="w-[50px] h-[50px] mr-[8px]" src={HomeLogo}></img>
         </a>
         <a href='/'>
           <div className="text-black font-bold">Gator Pulse - Connecting Gators</div>
