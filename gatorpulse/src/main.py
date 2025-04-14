3a81b20e0f0543b25d5bcdcc9833b782fe97f7e0
kaimcfarlane <kaimcfarlane43@gmail.com>
2025-04-14 10:10:59 -0400

    Update main.py file

--- gatorpulse/src/main.py
+++ gatorpulse/src/main.py
@@ -1,45 +1,29 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_cors import cross_origin
+import firebase_admin
+from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1 import FieldFilter
 
 app = Flask(__name__)
-cors = CORS(app, origins="*")
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate(
-    "../servicekey.json"
-)  # Obtain private key from firebase and rename it to "servicekey.json"
+# Apply CORS to all routes with the correct origin
+CORS(
+    app,
+    resources={r"/*": {"origins": "http://localhost:5173"}},
+    supports_credentials=True,
+)
+
+# Initialize Firebase
+cred = credentials.Certificate("../servicekey.json")
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
-
 ref = db.collection("users")
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.get_json()
-
-    usernameCheck = ref.where(
-        filter=FieldFilter("username", "==", data.get("username"))
-    ).get()
-    passCheck = ref.where(
-        filter=FieldFilter("username", "==", data.get("username"))
-    ).get()
-
-    if len(usernameCheck) == 0:
-        print("Username doesn't exist!")
-        return jsonify({"message": "failure!"})
-    else:
-        passcode = passCheck[0].to_dict().get("password")
-        if data.get("password") != passcode:
-            print("Password doesn't match!")
-            return jsonify({"message": "failure!"})
-        print("Logging in...")
-        return jsonify({"message": "success!"})
+@app.route("/")
+def home():
+    return "Server is up and running!"
 
 
 @app.route("/signup", methods=["POST"])
@@ -47,17 +31,14 @@ def signup():
     data = request.get_json()
     print(data.get("username"), data.get("password"), data.get("email"))
 
-    # Need to add encryption and email verification
     usernameCheck = ref.where(
         filter=FieldFilter("username", "==", data.get("username"))
     ).get()
     emailCheck = ref.where(filter=FieldFilter("email", "==", data.get("email"))).get()
 
     if len(usernameCheck) > 0:
-        print("Username already exists!")
         return jsonify({"message": "failure!"})
     else:
-        print("Creating new user...")
         ref.add(
             {
                 "createdAt": firestore.SERVER_TIMESTAMP,
@@ -66,7 +47,7 @@ def signup():
                 "password": data.get("password"),
             }
         )
-        return jsonify({"message": "success!"})
+        return jsonify({"message": "success!"}), 200
 
 
 if __name__ == "__main__":
