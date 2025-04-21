27287798fbf9bde8d76a49d021d82311a2d657a8
23jiyadurai <jeevan.iyadurai@gmail.com>
2025-04-21 01:33:23 -0400

    Adds profiles

--- gatorpulse/src/components/Header.jsx
+++ gatorpulse/src/components/Header.jsx
@@ -2,22 +2,28 @@
 import PropTypes from 'prop-types';
 import HomeLogo from '../../public/GatorPulseLogo2.png'
 import DateSelector from './DateSelector'
+import { Link } from 'react-router-dom'
+import { useUser } from './UserContext'
 
 export default function Header({page, selectedDate, setSelectedDate}) {
+  const {user, setUser} = useUser();
   return (
   <section className="flex flex-row justify-between items-center bg-white h-[7.38vh] fixed w-full z-[1] top-0 shadow-md px-4">
     {/* Left: Logo and Title */}
     <div className="flex items-center gap-2">
-      <a href="/">
+      <Link to="/">
         <img className="w-[50px] h-[50px]" src={HomeLogo} alt="Logo" />
-      </a>
-      <a href="/">
+      </Link>
+      <Link to="/">
         <div className="text-black font-bold text-lg">Gator Pulse - Connecting Gators</div>
-      </a>
+      </Link>
+      <Link to={`/profile/${user}`}>
+        <div className="text-black font-bold text-lg">Profile</div>
+      </Link>
     </div>
 
     {/* Right: Date Selector (if not on home page) */}
-    {page !== 'home' && (
+    {page == 'feed' && (
       <div className="flex items-center">
         <DateSelector selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
       </div>
--- gatorpulse/src/main.py
+++ gatorpulse/src/main.py
@@ -1,24 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_cors import CORS
 from flask_cors import cross_origin
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import firestore
 from google.cloud.firestore_v1 import FieldFilter
+import user, database
+import googlemaps
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
 # Apply CORS to all routes with the correct origin
-CORS(
+cors = CORS(
     app,
-    resources={r"/*": {"origins": "http://localhost:5173"}},
-    supports_credentials=True,
+    # resources={r"*": {"origins": "http://localhost:5173/*"}},
+    origins=["http://localhost:5173", "http://localhost:5173/profile", "http://localhost:5173/makePost"],
+    supports_credentials=True
 )
 
 # Initialize Firebase
-cred = credentials.Certificate("../servicekey.json")
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate("../servicekey.json")
+# firebase_admin.initialize_app(cred)
+load_dotenv()
+api_key = os.environ.get("GOOGLE-API")
+gmaps = googlemaps.Client(key=api_key)
+
 db = firestore.client()
 ref = db.collection("users")
+profs = db.collection("profiles")
+posts = db.collection("posts")
+
+default_pfp = "./assets/blank-pfp.png"
 
 
 @app.route("/")
@@ -37,7 +51,7 @@ def signup():
     emailCheck = ref.where(filter=FieldFilter("email", "==", data.get("email"))).get()
 
     if len(usernameCheck) > 0:
-        return jsonify({"message": "failure!"})
+        return jsonify({"message": "signup failure!"})
     else:
         ref.add(
             {
@@ -45,9 +59,86 @@ def signup():
                 "email": data.get("email"),
                 "username": data.get("username"),
                 "password": data.get("password"),
+                "biography": data.get("biography"),
+                "pfp": "none"
             }
         )
-        return jsonify({"message": "success!"}), 200
+        profs.add(
+            {
+                "username": data.get("username"),
+                "displayname": data.get("username"),
+                "biography": "Write something about yourself",
+                "pfp": "none"
+            }
+        )
+        return jsonify({"message": "signup success!"})
+    
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    usernameCheck = ref.where(
+        filter=FieldFilter("username", "==", data.get("username"))
+    ).get()
+    passCheck = ref.where(
+        filter=FieldFilter("username", "==", data.get("username"))
+    ).get()
+
+    if len(usernameCheck) == 0:
+        print("Username doesn't exist!")
+        return jsonify({"message": "failure!"})
+    else:
+        passcode = passCheck[0].to_dict().get("password")
+        if data.get("password") != passcode:
+            print("Password doesn't match!")
+            return jsonify({"message": "login failure!"})
+        print("Logging in...")
+        return jsonify({"message": "login success!"})
+    
+@app.route("/images/<filename>", methods=["GET"])
+def get_images(filename):
+    return send_from_directory(directory='assets', path=filename)
+
+# For testing profile settings
+@app.route("/profile/<targetuser>", methods=["POST", "GET"])
+def profile(targetuser):
+    print(f"Got request from {targetuser}")
+    # target_user = database.getUser(target_user)
+    # target_user = target_user.User(target_user)
+    target_profile = database.getProfile(targetuser)
+    
+    data_dict = target_profile.to_dict()
+
+    image_url = url_for('get_images', filename="blank-pfp.png")
+    if data_dict['pfp'] != "none":
+        image_url = data_dict['pfp']
+
+    # testUser.updateBio("Hello! My name is Adam Apple!")
+
+    response = jsonify({
+        "displayname": data_dict['displayname'],
+        "username": data_dict['username'],
+        "biography": data_dict['biography'],
+        "pfp_url": image_url
+    })
+    return response
+
+@app.route("/makePost", methods=['POST'])
+def make_post():
+    data = request.get_json()
+    candidates = gmaps.find_place(input=data.get("location"), input_type="textquery", fields=['place_id']).get('candidates')
+    loc = candidates[0] if candidates else ""
+    posts.add(
+        {
+            "username": data.get("username"),
+            "description": data.get("description"),
+            "date": data.get("date"),
+            "image": data.get("image"),
+            "location": loc,
+        }
+    )
+    return jsonify({"message": "successfully posted"})
+
 
 
 if __name__ == "__main__":
